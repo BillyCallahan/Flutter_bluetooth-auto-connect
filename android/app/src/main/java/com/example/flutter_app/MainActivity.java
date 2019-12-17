8e73dfaa83f5484f37ee01281a928d6318d3faaf
Audric Manaud <audric.manaud@gmail.com>
2019-12-17 12:26:41 +0100

    [+]update dart and java code. connection remaining

--- android/app/src/main/java/com/example/flutter_app/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_app/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.flutter_app;
 
-import android.bluetooth.le.BluetoothLeAdvertiser;
-import android.content.ContextWrapper;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.BatteryManager;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.bluetooth.BluetoothAdapter;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodChannel;
@@ -17,47 +19,119 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+@TargetApi(5)
 public class MainActivity extends FlutterActivity {
-  private static final String BATTERY_CHANNEL = "samples.flutter.io/bluetooth";
-  private final static int REQUEST_ENABLE_BT = 1;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    private static final String BATTERY_CHANNEL = "samples.flutter.io/bluetooth";
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+    private BluetoothDevice octavioDevice;
+    private MethodCall pendingCall;
+    private Result pendingResult;
 
-    new MethodChannel(getFlutterView(), BATTERY_CHANNEL).setMethodCallHandler(
-        new MethodCallHandler() {
-          @Override
-          public void onMethodCall(MethodCall call, Result result) {
-           if (call.method.equals("getBluetoothAdapter")) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
 
-              BluetoothAdapter bluetoothAdapter = getBluetoothAdapter();
-              if (bluetoothAdapter != null) {
+        new MethodChannel(getFlutterView(), BATTERY_CHANNEL).setMethodCallHandler(
+                new MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall call, Result result) {
 
-                if (!bluetoothAdapter.isEnabled()) {
-                  if (VERSION.SDK_INT >= VERSION_CODES.ECLAIR) {
-                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                    result.success(1);
-                  }
+                        switch (call.method){
+                            case "isBluetoothAvailable":
+                                result.success(btAdapter != null);
+                                break;
+
+                            case "isBluetoothEnabled":
+                                result.success(btAdapter.isEnabled());
+                                break;
+
+                            case "enableBluetooth":
+                                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                                pendingResult = result;
+                                break;
+
+                            case "startScan":
+                                startScan();
+                                pendingResult = result;
+                                break;
+
+                            case "stopScan":
+                                stopScan();
+                                break;
+
+                             default:
+                                 result.notImplemented();
+                        }
+
+                    }
                 }
-              } else {
-                result.error("UNAVAILABLE", "Bluetooth adapter not available", null);
-              }
-
-            } else {
-              result.notImplemented();
-            }
-          }
-        }
-      );
-  }
-
-  private BluetoothAdapter getBluetoothAdapter() {
-    if (VERSION.SDK_INT >= VERSION_CODES.ECLAIR) {
-      return BluetoothAdapter.getDefaultAdapter();
+        );
     }
-    return null;
-  }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        stopScan();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == REQUEST_ENABLE_BT)
+            pendingResult.success(resultCode != 0);
+
+    }
+
+    @TargetApi(5)
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                if ( isDeviceMatching(device.getName()) ) {
+                    octavioDevice = device;
+                    btAdapter.cancelDiscovery();
+                    unregisterReceiver(receiver);
+                    pendingResult.success(true);
+                }
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                System.out.println("Finiiii");
+            }
+        }
+    };
+
+    private Boolean isDeviceMatching(String deviceName){
+        //prevent from getting a null deviceName, happens sometimes
+        if (deviceName == null) return false;
+
+        String pattern = "^Octavio_[a-zA-Z0-9]{4}";
+        Pattern p = Pattern.compile(pattern);
+        Matcher matcher = p.matcher(deviceName);
+
+        return matcher.find();
+    }
+
+    private void startScan() {
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(receiver, filter);
+        if (btAdapter.isDiscovering()) {
+            btAdapter.cancelDiscovery();
+        }
+        btAdapter.startDiscovery();
+    }
+
+    private void stopScan() {
+        if (btAdapter.isDiscovering()) {
+            btAdapter.cancelDiscovery();
+        }
+        unregisterReceiver(receiver);
+    }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PlatformChannel extends StatefulWidget {
   @override
@@ -14,38 +15,95 @@ class PlatformChannel extends StatefulWidget {
 
 class _PlatformChannelState extends State<PlatformChannel> {
 
-  static const MethodChannel methodChannel =
-  MethodChannel('samples.flutter.io/bluetooth');
+  static const MethodChannel bluetoothChannel = MethodChannel('samples.flutter.io/bluetooth');
 
-  String _batteryLevel = 'Battery level: unknown.';
-  String _bluetoothAdapter = 'Bluetooth adapter: unknown';
-
-  Future<void> _getBatteryLevel() async {
-    String batteryLevel;
-    try {
-      final int result = await methodChannel.invokeMethod('getBatteryLevel');
-      batteryLevel = 'Battery level: $result%.';
-    } on PlatformException {
-      batteryLevel = 'Failed to get battery level.';
-    }
-    setState(() {
-      _batteryLevel = batteryLevel;
-    });
+  void _handleClick() {
+    _connectToNearbyDevice();
   }
 
-  Future<void> _getBluetoothAdapter() async {
-    String bluetoothAdapter;
-    try {
-      final int result = await methodChannel.invokeMethod('getBluetoothAdapter');
-      bluetoothAdapter = 'Battery level: $result%.';
-    } on PlatformException {
-      bluetoothAdapter = 'Failed to get battery level.';
-    }
-    setState(() {
-      _bluetoothAdapter = bluetoothAdapter;
-    });
+  void _showSnackBar(String text) {
+    Scaffold.of(context).showSnackBar(new SnackBar(
+        content: new Text(text),
+        action: SnackBarAction(
+          label: 'Got it',
+          onPressed: () {},
+        ),
+    ));
   }
 
+  void _connectToNearbyDevice() async {
+
+    try {
+      if (!await _isBluetoothAvailable()) {
+        _showSnackBar("No bluetooth adapter found...");
+        return;
+      }
+
+      if (!await _isBluetoothEnabled()) {
+        if (!await _enableBluetooth()) {
+          _showSnackBar("Why won't you let me ?");
+          return;
+        }
+      }
+
+      if (!await _checkPermissions()) {
+        _showSnackBar("Permission denied");
+        return;
+      }
+
+      if (!await _startScan()) {
+        _showSnackBar("No Octavio device found");
+        return;
+      }
+    }
+    catch (e) { _showSnackBar(e.message); }
+
+  }
+
+  Future<bool> _isBluetoothAvailable() async {
+    try {
+      return await bluetoothChannel.invokeMethod('isBluetoothAvailable');
+    }
+    on PlatformException catch (e) { throw e; }
+  }
+
+  Future<bool> _isBluetoothEnabled() async {
+    try {
+      return await bluetoothChannel.invokeMethod('isBluetoothEnabled');
+    }
+    on PlatformException catch (e) { throw e; }
+  }
+
+  Future<bool> _enableBluetooth() async {
+    try {
+      return await bluetoothChannel.invokeMethod('enableBluetooth');
+    }
+    on PlatformException catch (e) { throw e; }
+  }
+
+  Future<bool> _startScan() async {
+    try {
+      return await bluetoothChannel.invokeMethod('startScan')
+                                .timeout(const Duration(seconds: 10), onTimeout: _onTimeOut);
+    }
+    on PlatformException catch (e) { throw e; }
+  }
+
+  bool _onTimeOut() {
+    bluetoothChannel.invokeMethod('stopScan');
+    return false;
+  }
+
+  Future<bool> _checkPermissions() async {
+    PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.location);
+
+    if (permission == PermissionStatus.denied) {
+      Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler().requestPermissions([PermissionGroup.location]);
+      return permissions[PermissionGroup.location] == PermissionStatus.disabled ||
+             permissions[PermissionGroup.location] == PermissionStatus.granted;
+    }
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,12 +114,11 @@ class _PlatformChannelState extends State<PlatformChannel> {
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(_bluetoothAdapter, key: const Key('Battery level label')),
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: RaisedButton(
-                  child: const Text('Refresh'),
-                  onPressed: _getBluetoothAdapter,
+                  child: const Text('Connect to a nearby device'),
+                  onPressed: _handleClick,
                 ),
               ),
             ],
@@ -73,5 +130,15 @@ class _PlatformChannelState extends State<PlatformChannel> {
 }
 
 void main() {
-  runApp(MaterialApp(home: PlatformChannel()));
+  runApp(
+      MaterialApp(
+          title: 'Bluetooth Auto Connect',
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text('Bluetooth Auto Connect'),
+            ),
+            body: PlatformChannel(),
+          ),
+      )
+  );
 }
